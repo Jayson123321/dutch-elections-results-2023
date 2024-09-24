677d942a6fa1670c25c3eb89ff0c804a7ae74605
Jayson <jayson.haverkamp@hva.nl>
2024-09-24 12:54:23 +0200

    feat test

--- election-frontend-vue/src/components/Registration.vue
+++ election-frontend-vue/src/components/Registration.vue
@@ -16,8 +16,8 @@ export default defineComponent({
       <label for="password">Password</label>
       <input type="password" id="password" required>
       <label for="email">Email</label>
-      <input type="email" id="emaixl" required>
-      <button type="submit">Register</button>
+      <input type="email" id="email" required>
+      <button type="submit">Registerr</button>
     </form>
   </main>
 </template>
