96b3d9b4a574ccfa5ee1314fd9ac3930a9aa3263
Jayson <jayson.haverkamp@hva.nl>
2024-09-26 21:44:24 +0200

    testcommit

--- backend-java/src/main/java/com/election/backendjava/LoginRequest.java
+++ backend-java/src/main/java/com/election/backendjava/LoginRequest.java
@@ -9,7 +9,7 @@ public class LoginRequest {
     }
 
     public void setUsername(String username) {
-        this.usersname = username;
+        this.username = username;
     }
 
     public String getPassword() {
