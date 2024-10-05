51765b2bc953b5beca42d898f26ac99b9f365844
Jayson <jayson.haverkamp@hva.nl>
2024-10-05 21:16:09 +0200

    added header and footer

--- election-frontend-vue/src/router/index.ts
+++ election-frontend-vue/src/router/index.ts
@@ -1,15 +1,22 @@
+// src/router/index.ts
 import { createRouter, createWebHistory } from 'vue-router';
-import loginPage from '../components/login.vue';
-import home from '../home.vue';
+import LoginPage from '../components/login.vue';
+import Home from '../home.vue';
 import Registration from "@/components/Registration.vue";
+import PartyVotesTable from "@/components/PartyVotesTable.vue";
+import ManagingAuthority from "@/components/ManagingAuthority.vue";
+
 const routes = [
-    { path: '/', component: loginPage },
-    { path: '/home', component: home },
-    { path: '/Registration', component: Registration },
+    { path: '/', component: LoginPage },
+    { path: '/home', component: Home },
+    { path: '/registration', component: Registration },
+    { path: '/party-votes', component: PartyVotesTable },
+    { path: '/managing-authorities', component: ManagingAuthority }
 ];
-//test
+
 const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
 export default router;
