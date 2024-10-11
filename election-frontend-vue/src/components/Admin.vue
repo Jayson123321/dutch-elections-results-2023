02caa93dac141c1e38bddca55a74a4d452a7de49
khaouia <khaouia@hva.nl>
2024-10-11 20:02:32 +0200

    #22 feat: basic page layout

--- election-frontend-vue/src/components/Admin.vue
+++ election-frontend-vue/src/components/Admin.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-
+  <div class="admin-page">
+    <h1>Admin Dashboard</h1>
+    <p>Welkom op de adminpagina.</p>
+  </div>
 </template>
-
-<style scoped>
-
+<style>
+.admin-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  margin-top: 75px;
+}
 </style>
