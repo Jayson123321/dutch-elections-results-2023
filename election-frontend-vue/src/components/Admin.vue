5688b5eca11ff475f4ff075571b7a3be46b3e387
khaouia <khaouia@hva.nl>
2024-10-11 20:05:42 +0200

    #22 feat: importing header & footer

--- election-frontend-vue/src/components/Admin.vue
+++ election-frontend-vue/src/components/Admin.vue
@@ -1,7 +1,22 @@
+<script>
+import FooterComponent from './FooterComponent.vue';
+import HeaderComponent from './HeaderComponent.vue';
+export default {
+  name: "Admin",
+  components: {
+    FooterComponent,
+    HeaderComponent
+  }
+}
+</script>
 <template>
-  <div class="admin-page">
-    <h1>Admin Dashboard</h1>
-    <p>Welkom op de adminpagina.</p>
+  <div>
+    <HeaderComponent />
+    <div class="admin-page">
+      <h1>Admin Dashboard</h1>
+      <p>Welkom op de adminpagina.</p>
+    </div>
+    <FooterComponent />
   </div>
 </template>
 <style>
