d16d926ef75fda4d2479ef8a0b143deb3b6df9c8
khaouia <khaouia@hva.nl>
2024-10-13 16:28:40 +0200

    #22 feat: find all admin service

--- backend-java/src/main/java/com/election/backendjava/AdminService.java
+++ backend-java/src/main/java/com/election/backendjava/AdminService.java
@@ -1,4 +1,17 @@
 package com.election.backendjava;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AdminService {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    public List<Admin> getAllAdmins() {
+        return adminRepository.findAll();
+    }
 }
