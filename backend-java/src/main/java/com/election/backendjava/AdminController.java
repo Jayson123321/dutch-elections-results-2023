ed0385ee0d20a12849c89e26c26ae3c3d6f6bbbe
khaouia <khaouia@hva.nl>
2024-10-13 16:26:22 +0200

    #22 feat: get all method

--- backend-java/src/main/java/com/election/backendjava/AdminController.java
+++ backend-java/src/main/java/com/election/backendjava/AdminController.java
@@ -1,15 +1,26 @@
 package com.election.backendjava;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
 
+    @Autowired
+    private AdminService adminService;
+
     @GetMapping("/test")
     public String test() {
-        return "test API for admin";
+        return "Admin test API!";
+    }
+
+    @GetMapping("/all")
+    public List<Admin> getAllAdmins() {
+        return adminService.getAllAdmins();
     }
 }
