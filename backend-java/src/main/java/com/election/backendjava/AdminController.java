e07bf8906d73bc59e5a5c2d38053c591e2b4fad1
khaouia <khaouia@hva.nl>
2024-10-13 16:20:37 +0200

    #22 feat: test endpoint

--- backend-java/src/main/java/com/election/backendjava/AdminController.java
+++ backend-java/src/main/java/com/election/backendjava/AdminController.java
@@ -8,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/admin")
 public class AdminController {
 
+    @GetMapping("/test")
+    public String test() {
+        return "test API for admin";
+    }
 }
