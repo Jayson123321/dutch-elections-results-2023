5dbbcd6d33fd05a3ad67a91ee510eb54c76b0eda
khaouia <khaouia@hva.nl>
2024-10-13 16:18:36 +0200

    #22 feat: Admin RestController

--- backend-java/src/main/java/com/election/backendjava/AdminController.java
+++ backend-java/src/main/java/com/election/backendjava/AdminController.java
@@ -1,4 +1,11 @@
 package com.election.backendjava;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
 public class AdminController {
+
 }
