c19c4d72b26e8a0bc9bedee638eb155a3f4f24c1
khaouia <khaouia@hva.nl>
2024-10-14 23:51:03 +0200

    #22 feat: statement if no users found

--- election-frontend-vue/src/components/Admin.vue
+++ election-frontend-vue/src/components/Admin.vue
@@ -1,11 +1,27 @@
 <script>
 import FooterComponent from './FooterComponent.vue';
 import HeaderComponent from './HeaderComponent.vue';
+import axios from 'axios';
+
 export default {
   name: "Admin",
   components: {
     FooterComponent,
     HeaderComponent
+  },
+  data() {
+    return {
+      users: []
+    };
+  },
+  mounted() {
+    axios.get('/api/users/all')
+        .then(response => {
+          this.users = response.data.map(user => ({ username: user.username }));
+        })
+        .catch(error => {
+          console.error('Er is een fout opgetreden bij het ophalen van de gebruikersgegevens:', error);
+        });
   }
 }
 </script>
@@ -13,9 +29,19 @@ export default {
   <div>
     <HeaderComponent />
     <div class="admin-page">
-      <h2>John Doe</h2>
-      <h1>Admin</h1>
-      <h3>Analytics</h3>
+      <h1>Admin Dashboard</h1>
+      <p>Welkom op de adminpagina.</p>
+      <div v-if="users.length > 0">
+        <h2>Users:</h2>
+        <ul>
+          <li v-for="user in users" :key="user.username">
+            {{ user.username }}
+          </li>
+        </ul>
+      </div>
+      <div v-else>
+        <p>Geen users gevonden.</p>
+      </div>
     </div>
     <FooterComponent />
   </div>
