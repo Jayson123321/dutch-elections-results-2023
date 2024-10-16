e02940fe0a48d48e161c76451d4b57962fe45b1e
Darya Masoumi <Darya.masoumi@hva.nl>
2024-10-16 16:28:37 +0200

    added to package repositories

--- backend-java/src/main/java/com/election/backendjava/repositories/TotalVoteRepository.java
+++ backend-java/src/main/java/com/election/backendjava/repositories/TotalVoteRepository.java
@@ -1,5 +1,6 @@
-package com.election.backendjava;
+package com.election.backendjava.repositories;
 
+import com.election.backendjava.entities.TotalVote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TotalVoteRepository extends JpaRepository<TotalVote,Long> {
