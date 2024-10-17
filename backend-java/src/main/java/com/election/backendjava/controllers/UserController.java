d0913783110eea480f4841ab4c56b050314d1d82
khaouia <khaouia@hva.nl>
2024-10-17 22:03:52 +0200

    feat: controller to get endpoints for usernames

--- backend-java/src/main/java/com/election/backendjava/controllers/UserController.java
+++ backend-java/src/main/java/com/election/backendjava/controllers/UserController.java
@@ -1,4 +1,26 @@
 package com.election.backendjava.controllers;
 
+import com.election.backendjava.User;
+import com.election.backendjava.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all")
+    public List<String> getAllUsernames() {
+        return userService.getAllUsers().stream()
+                .map(User::getUsername)
+                .collect(Collectors.toList());
+    }
 }
