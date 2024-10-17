a09bcb51902b3a865470a4af5ab37ee6c94b25e8
Darya Masoumi <Darya.masoumi@hva.nl>
2024-10-17 22:17:34 +0200

    Added percentage calculation to TotalVoteController for election results

--- backend-java/src/main/java/com/election/backendjava/controllers/TotalVoteController.java
+++ backend-java/src/main/java/com/election/backendjava/controllers/TotalVoteController.java
@@ -1,5 +1,6 @@
 package com.election.backendjava.controllers;
 
+import com.election.backendjava.dto.TotalVoteWithPercentageDTO;
 import com.election.backendjava.entities.TotalVote;
 import com.election.backendjava.repositories.TotalVoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/results")
@@ -21,7 +24,23 @@ public class TotalVoteController {
     }
 
     @GetMapping
-    public List<TotalVote> getAllTotalVotes() {
-        return totalVoteRepository.findAll();
+    public List<TotalVoteWithPercentageDTO> getAllTotalVotes() {
+        List<TotalVote> totalVotes = totalVoteRepository.findAll();
+
+        // Bereken het totaal aantal stemmen
+        int sumVotes = totalVotes.stream().mapToInt(TotalVote::getTotalVotes).sum();
+
+        // Maak een lijst van DTO's met percentages
+        List<TotalVoteWithPercentageDTO> result = new ArrayList<>();
+        for (TotalVote vote : totalVotes) {
+            double percentage = sumVotes > 0 ? (double) vote.getTotalVotes() / sumVotes * 100 : 0;
+            result.add(new TotalVoteWithPercentageDTO(vote.getAffiliationName(), vote.getTotalVotes(), percentage));
+        }
+
+        return result;
     }
 }
+
+//    public List<TotalVote> getAllTotalVotes() {
+//        return totalVoteRepository.findAll();
+//    }
