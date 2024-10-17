8357a58f2d455afc6ff86ba2b83016e6b91f7228
khaouia <khaouia@hva.nl>
2024-10-17 22:00:34 +0200

    feat: finding all users

--- backend-java/src/main/java/com/election/backendjava/services/UserService.java
+++ backend-java/src/main/java/com/election/backendjava/services/UserService.java
@@ -1,4 +1,19 @@
 package com.election.backendjava.services;
 
+import com.election.backendjava.entities.User;
+import com.election.backendjava.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
