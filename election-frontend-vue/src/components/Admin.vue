0a154d83c1ecf9b68285cc2b6cc2ae99cadf3e56
khaouia <khaouia@hva.nl>
2024-10-19 19:27:48 +0200

    fix: deleted section on accident

--- election-frontend-vue/src/components/Admin.vue
+++ election-frontend-vue/src/components/Admin.vue
@@ -1,3 +1,78 @@
+<script>
+import FooterComponent from './FooterComponent.vue';
+import HeaderComponent from './HeaderComponent.vue';
+import axios from 'axios';
+
+export default {
+  name: "Admin",
+  components: {
+    FooterComponent,
+    HeaderComponent
+  },
+  data() {
+    return {
+      users: []
+    };
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+  methods: {
+    fetchUsers() {
+      axios.get('http://localhost:8080/api/users/all', { responseType: 'json' })
+          .then(response => {
+            this.users = response.data;
+          })
+          .catch(error => {
+            console.error('Er is een fout opgetreden bij het ophalen van de gebruikersgegevens:', error);
+          });
+    },
+    deleteUser(userId) {
+      if (confirm('Weet je zeker dat je deze gebruiker wilt verwijderen?')) {
+        axios.delete(`http://localhost:8080/api/users/${userId}`)
+            .then(() => {
+              alert('Gebruiker succesvol verwijderd.');
+              this.fetchUsers();
+            })
+            .catch(error => {
+              console.error('Er is een fout opgetreden bij het verwijderen van de gebruiker:', error);
+            });
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <HeaderComponent />
+    <div class="admin-page">
+      <div v-if="users.length > 0">
+        <table>
+          <thead>
+          <tr>
+            <th>Gebruikersnaam</th>
+            <th>Acties</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <td>{{ user.username }}</td>
+            <td>
+              <button @click="deleteUser(user.id)">Verwijder</button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-else>
+        <p>Geen users gevonden.</p>
+      </div>
+    </div>
+    <FooterComponent />
+  </div>
+</template>
+
 <style scoped>
 .admin-page {
   padding: 20px;
