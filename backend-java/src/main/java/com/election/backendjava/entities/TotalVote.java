60721d86e8d8ac6549ae04e18031091e3df0a37f
Darya Masoumi <Darya.masoumi@hva.nl>
2024-10-20 22:26:45 +0200

    deleted column annotation in total votes class

--- backend-java/src/main/java/com/election/backendjava/entities/TotalVote.java
+++ backend-java/src/main/java/com/election/backendjava/entities/TotalVote.java
@@ -8,15 +8,14 @@ public class TotalVote {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-
-    @Column(name = "affiliation_name")
     private String affiliationName;
 
-    @Column(name = "total_votes")
     private Integer totalVotes;
 
-    @Column(name = "affiliation_id")
     private Integer affiliationId;
+
+
+    //getters and setters
 
     public String getAffiliationName() {
         return affiliationName;
