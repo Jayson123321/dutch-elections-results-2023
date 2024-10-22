4b7f197d820cd97f092f73095bde6e10d5434d8b
Ruben <ruben.plat@hva.nl>
2024-10-22 11:42:41 +0100

    candidaten totaal uitslagen in de database gezet.

--- backend-java/src/main/java/com/election/backendjava/processors/CandidateVoteParser.java
+++ backend-java/src/main/java/com/election/backendjava/processors/CandidateVoteParser.java
@@ -8,6 +8,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
+import jakarta.annotation.PostConstruct;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
@@ -18,10 +19,14 @@ public class CandidateVoteParser {
     @Autowired
     private CandidateVotesService candidateVotesService;
 
-    public void parseAndSaveVotes(String filePath) {
+    // Pad naar het XML-bestand (pas dit aan naar de locatie van jouw bestand)
+    private String xmlFilePath = "C:\\Users\\ruben\\OneDrive\\Documenten\\hva\\Totaaltelling_TK2023.eml.xml";
+
+    @PostConstruct
+    public void parseAndSaveVotes() {
         try {
             // 1. Open de XML file en parse het document
-            File xmlFile = new File(filePath);
+            File xmlFile = new File(xmlFilePath);
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document document = builder.parse(xmlFile);
@@ -37,29 +42,33 @@ public class CandidateVoteParser {
                 Element selectionElement = (Element) selectionNodes.item(i);
 
                 // 5. Haal de "CandidateIdentifier" en "ValidVotes" op
-                String candidateIdentifier = selectionElement
-                        .getElementsByTagName("CandidateIdentifier")
-                        .item(0)
-                        .getAttributes()
-                        .getNamedItem("ShortCode")
-                        .getNodeValue();
+                NodeList candidateIdentifierNodes = selectionElement.getElementsByTagName("CandidateIdentifier");
+                if (candidateIdentifierNodes.getLength() > 0) {
+                    String candidateIdentifier = candidateIdentifierNodes.item(0)
+                            .getAttributes()
+                            .getNamedItem("ShortCode")
+                            .getNodeValue();
 
-                int validVotes = Integer.parseInt(selectionElement
-                        .getElementsByTagName("ValidVotes")
-                        .item(0)
-                        .getTextContent());
+                    int validVotes = Integer.parseInt(selectionElement
+                            .getElementsByTagName("ValidVotes")
+                            .item(0)
+                            .getTextContent());
 
-                // 6. Maak het CandidateVotes object aan en sla de data op
-                CandidateVotes candidateVotes = new CandidateVotes();
-                candidateVotes.setCandidateIdentifier(candidateIdentifier);
-                candidateVotes.setValidVotes(validVotes);
+                    // 6. Maak het CandidateVotes object aan en sla de data op
+                    CandidateVotes candidateVotes = new CandidateVotes();
+                    candidateVotes.setCandidateIdentifier(candidateIdentifier);
+                    candidateVotes.setValidVotes(validVotes);
 
-                // 7. Sla het object op via de service
-                candidateVotesService.saveCandidateVotes(candidateVotes);
+                    // 7. Sla het object op via de service
+                    candidateVotesService.saveCandidateVotes(candidateVotes);
+                } else {
+                    System.err.println("No CandidateIdentifier found in Selection " + i);
+                }
             }
-        } catch (Exception e) {
+            } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
 
