c4f904565f2fbd817554e6e3ba05d41f7c798e5b
Jayson <jayson.haverkamp@hva.nl>
2024-11-11 22:53:49 +0100

    Moved file to 'entities'

--- backend-java/src/main/java/com/election/backendjava/processors/RssParser.java
+++ backend-java/src/main/java/com/election/backendjava/processors/RssParser.java
@@ -1,6 +1,6 @@
 package com.election.backendjava.processors;
 
-import com.election.backendjava.Article;
+import com.election.backendjava.entities.Article;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
