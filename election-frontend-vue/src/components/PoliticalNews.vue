d5a0ef71cc364975f73ec810acdd46b8d60b4a61
Jayson <jayson.haverkamp@hva.nl>
2024-11-12 16:05:23 +0100

    Styling Component politcalNews

--- election-frontend-vue/src/components/PoliticalNews.vue
+++ election-frontend-vue/src/components/PoliticalNews.vue
@@ -1,24 +1,65 @@
 <template>
-  <div>
-    <h2>Laatste politieke nieuws</h2>
-    <ul v-if="news.length">
-      <li v-for="(article, index) in news" :key="index" :href="article.link">
+  <div class="news-container">
+    <h2>De laatste politieke nieuws</h2>
+    <h4>(Bron: Nu.nl)</h4>
+    <ul>
+      <li v-for="(article, index) in news" :key="index">
         <a :href="article.link" target="_blank">{{ article.title }}</a>
       </li>
     </ul>
-    <p v-else>Geen nieuws beschikbaar</p>
   </div>
 </template>
-<style>
+
+<style scoped>
+h4 {
+  margin-bottom: 10px;
+  font-size: 0.875rem;
+  font-weight: 400;
+  color: #888;
+  text-align: center;
+}
+.news-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(151, 151, 151, 0.1);
+}
+
+h2 {
+  font-size: 2rem;
+  text-align: center;
+  margin-bottom: 20px;
+}
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
-  margin-bottom: 1rem;
+  margin-bottom: 15px;
+  padding: 15px;
+  border-radius: 8px;
+  transition: transform 0.3s, background-color 0.3s, box-shadow 0.3s;
+  box-shadow: 0 2px 4px rgba(154, 154, 154, 0.1);
 }
 
+li:hover {
+  transform: translateY(-5px);
+  box-shadow: 0 4px 8px rgba(143, 143, 143, 0.2);
+}
+
+a {
+  text-decoration: none;
+  font-weight: bold;
+}
+
+a:hover {
+  text-decoration: underline;
+}
 </style>
+
 <script>
 import axios from 'axios';
 
@@ -27,11 +68,11 @@ export default {
   data() {
     return {
       news: [],
+      showPoliticalNews: false
     };
   },
   mounted() {
     this.fetchNews();
-    // Interval om elke 5 minuten (300000 ms) nieuws op te halen
     setInterval(this.fetchNews, 300000);
   },
   methods: {
@@ -42,8 +83,7 @@ export default {
       } catch (error) {
         console.error("Error fetching news:", error);
       }
-    },
-  },
+    }
+  }
 };
-
 </script>
