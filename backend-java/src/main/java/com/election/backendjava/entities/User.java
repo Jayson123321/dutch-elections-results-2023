69b4323e335524ba4c751a93bea1708f068c0d45
Kaylin Bon <kaylin.bon@hva.nl>
2024-11-19 17:30:28 +0100

    made changes to pom.xml and added a jwtservice

--- backend-java/src/main/java/com/election/backendjava/entities/User.java
+++ backend-java/src/main/java/com/election/backendjava/entities/User.java
@@ -1,11 +1,15 @@
 package com.election.backendjava.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -50,5 +54,30 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
