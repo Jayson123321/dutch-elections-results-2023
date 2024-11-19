6cfe634061c54c55dad57b4650eb1bb2215ef654
Kaylin Bon <kaylin.bon@hva.nl>
2024-11-19 17:54:10 +0100

    added a jwt authentication filter and fixed some bugs

--- backend-java/src/main/java/com/election/backendjava/configs/ApplicationConfiguration.java
+++ backend-java/src/main/java/com/election/backendjava/configs/ApplicationConfiguration.java
@@ -1,6 +1,6 @@
 package com.election.backendjava.configs;
 
-import com.tericcabrel.authapi.repositories.UserRepository;
+import com.election.backendjava.repositories.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
