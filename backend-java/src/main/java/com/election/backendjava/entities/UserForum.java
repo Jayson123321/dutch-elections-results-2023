b4a2341c6326a172b333623fa89a362321d12913
Darya Masoumi <Darya.masoumi@hva.nl>
2024-11-19 11:07:05 +0100

    added user id to forum entity

--- backend-java/src/main/java/com/election/backendjava/entities/UserForum.java
+++ backend-java/src/main/java/com/election/backendjava/entities/UserForum.java
@@ -1,35 +1,53 @@
 package com.election.backendjava.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class UserForum {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    private Long id;
+    @Column(name = "forum_id", nullable = false, updatable = false)
+    private Long forumId;
 
-   private String username;
+   @Column(name = "title", nullable = false, updatable = false)
+   private String title;
+
+   @Column(name = "description", nullable = false, updatable = false)
+   private String description;
+
+   @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id" , nullable = false)
+   private User user;
 
    //getters and setters
-   public Long getId() {
-       return id;
+   public Long getForumId() {
+       return forumId;
+   }
+   public void setForumId(Long forumId) {
+       this.forumId = forumId;
    }
 
-   public void setId(Long id) {
-       this.id = id;
+    public User getUser() {
+       return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+   public String getTitle() {
+       return title;
+   }
+   public void setTitle(String title) {
+       this.title = title;
    }
 
-   public String getUsername() {
-       return username;
+   public String getDescription() {
+       return description;
    }
-
-   public void setUsername(String username) {
-       this.username = username;
+   public void setDescription(String description) {
+       this.description = description;
    }
-
 
 }
