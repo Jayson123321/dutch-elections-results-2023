0fa7d4b1188efb4b44eccd323327dfa3ec0b0a5a
Darya Masoumi <Darya.masoumi@hva.nl>
2024-11-20 16:43:54 +0100

    added forum service in the backend

---
+++ backend-java/src/main/java/com/election/backendjava/services/ForumService.java
@@ -0,0 +1,18 @@
+package com.election.backendjava.services;
+
+import com.election.backendjava.entities.UserForum;
+import com.election.backendjava.repositories.PostForumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ForumService {
+    @Autowired
+    private PostForumRepository postForumRepository;
+
+    public UserForum save(UserForum form) {
+        return  postForumRepository.save(form);
+    }
+
+}
