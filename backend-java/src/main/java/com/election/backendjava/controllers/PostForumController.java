5b1255d2a938be9257e73c68b0d650d53acf9356
Darya Masoumi <Darya.masoumi@hva.nl>
2024-11-25 17:08:14 +0100

    edited the forum controller

--- backend-java/src/main/java/com/election/backendjava/controllers/PostForumController.java
+++ backend-java/src/main/java/com/election/backendjava/controllers/PostForumController.java
@@ -2,19 +2,38 @@ package com.election.backendjava.controllers;
 
 import com.election.backendjava.entities.UserForum;
 import com.election.backendjava.repositories.PostForumRepository;
+import com.election.backendjava.services.ForumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/usersforum")
 public class PostForumController {
 
     @Autowired
-    private PostForumRepository postForumRepository;
+    private ForumService forumService;;
 
+    // stuur de forums naar database
     @PostMapping
     public UserForum saveUserForum(@RequestBody UserForum userForum) {
-        return postForumRepository.save(userForum);
+        return forumService.save(userForum);
     }
 
+    // Haal alle forums op
+//    @GetMapping
+//    public List<UserForum> getAllForums() {
+//        return postForumRepository.findAll();
+//    }
+//
+//    @PostMapping
+//    public UserForum createForum(@RequestBody UserForum userForum) {
+//        return forumService.createForum(userForum);
+//    }
+
+    @GetMapping
+    public List<UserForum> getAllForums() {
+        return forumService.getAllForums();
+    }
 }
