63a905aab8401cc49921a9b0844745c1c31fd426
Jayson <jayson.haverkamp@hva.nl>
2024-12-07 16:54:37 +0100

    Added new query in CandidateAuthorityVotesRepository - OrderByValidVotesDesc. To get results by votes

--- backend-java/src/main/java/com/election/backendjava/repositories/CandidateAuthorityVotesRepository.java
+++ backend-java/src/main/java/com/election/backendjava/repositories/CandidateAuthorityVotesRepository.java
@@ -10,4 +10,7 @@ import java.util.List;
 
 @Repository
 public interface CandidateAuthorityVotesRepository extends JpaRepository<CandidateAuthorityVotes, Long> {
-    List<CandidateAuthorityVotes> findByAffiliationIdAndAuthorityIdentifier(Long affiliationId, String authorityIdentifier);}
+    List<CandidateAuthorityVotes> findByAffiliationIdAndAuthorityIdentifier(Long affiliationId, String authorityIdentifier);
+
+    List<CandidateAuthorityVotes> findByAffiliationIdAndAuthorityIdentifierOrderByValidVotesDesc(Long affiliationId, String authorityId);
+}
