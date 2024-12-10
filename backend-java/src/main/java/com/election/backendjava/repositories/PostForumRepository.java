162d701cbca062d656d7a1f03d2b5bca7cb6c736
Darya Masoumi <Darya.masoumi@hva.nl>
2024-12-10 23:48:16 +0100

    added query To have the newest forum appear at the top of the page.

--- backend-java/src/main/java/com/election/backendjava/repositories/PostForumRepository.java
+++ backend-java/src/main/java/com/election/backendjava/repositories/PostForumRepository.java
@@ -2,7 +2,12 @@ package com.election.backendjava.repositories;
 
 import com.election.backendjava.entities.UserForum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostForumRepository extends JpaRepository<UserForum, Long> {
+    @Query("SELECT f FROM UserForum f ORDER BY f.createdAt DESC")
+    List<UserForum> findAllSortedByCreatedAt();
 
 }
