a7fb8e9d142a1daf2d7e6b2f0872e4b0508b789a
Darya Masoumi <Darya.masoumi@hva.nl>
2024-12-11 23:22:56 +0100

    added query in repository to delete the replies of the forum

--- backend-java/src/main/java/com/election/backendjava/repositories/ReplyRepository.java
+++ backend-java/src/main/java/com/election/backendjava/repositories/ReplyRepository.java
@@ -2,9 +2,17 @@ package com.election.backendjava.repositories;
 
 import com.election.backendjava.entities.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
     List<Reply> findByUserForum_ForumId(Long forumId);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Reply r WHERE r.userForum.forumId = : forumId")
+    void deleteByUserForum_ForumId(Long forumId);
 }
