70412bc06d95356ecc7a60972334bfede6474398
khaouia <khaouia@hva.nl>
2024-12-16 10:28:06 +0100

    entities and relations

--- backend-java/src/main/java/com/election/backendjava/entities/UnbanRequest.java
+++ backend-java/src/main/java/com/election/backendjava/entities/UnbanRequest.java
@@ -1,5 +1,32 @@
 package com.election.backendjava.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "unban_request")
 public class UnbanRequest {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
