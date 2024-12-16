3996786dcc863fe6cd59e8fbb423e42f25b2e355
Kaylin Bon <kaylin.bon@hva.nl>
2024-12-16 20:28:46 +0100

    removed unnessesary files

--- backend-java/src/main/java/com/election/backendjava/Login.java
+++ backend-java/src/main/java/com/election/backendjava/Login.java
@@ -1,34 +1,33 @@
-package com.election.backendjava;
-
-import com.election.backendjava.dto.LoginRequest;
-import com.election.backendjava.entities.User;
-import com.election.backendjava.repositories.UserRepository;
-import com.election.backendjava.services.JwtService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api")
-public class Login {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private JwtService jwtService;
-
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest userCredentials) {
-        User userFromDb = userRepository.findByEmailAndPassword(userCredentials.email(), userCredentials.password());
-
-        if (userFromDb != null) {
-            String tokenString = jwtService.encode(userFromDb.getUsername(), userFromDb.getId(), userFromDb.getRole());
-            return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenString)
-                    .body("{\"token\": \"" + tokenString + "\", \"message\": \"Login successful\"}");
-        } else {
-            return ResponseEntity.status(401).body("{\"message\": \"Login failed: Incorrect email or password\"}");
-        }
-    }
-}
+//package com.election.backendjava;
+//
+//import com.election.backendjava.dto.LoginRequest;
+//import com.election.backendjava.entities.User;
+//import com.election.backendjava.repositories.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class Login {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private JwtService jwtService;
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<String> login(@RequestBody LoginRequest userCredentials) {
+//        User userFromDb = userRepository.findByEmailAndPassword(userCredentials.email(), userCredentials.password());
+//
+//        if (userFromDb != null) {
+//            String tokenString = jwtService.encode(userFromDb.getUsername(), userFromDb.getId(), userFromDb.getRole());
+//            return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenString)
+//                    .body("{\"token\": \"" + tokenString + "\", \"message\": \"Login successful\"}");
+//        } else {
+//            return ResponseEntity.status(401).body("{\"message\": \"Login failed: Incorrect email or password\"}");
+//        }
+//    }
+//}
