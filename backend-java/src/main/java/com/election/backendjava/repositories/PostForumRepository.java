1904e4f9a7d227e49a24256e111285bbdefb09be
Darya Masoumi <Darya.masoumi@hva.nl>
2024-12-16 23:49:38 +0100

    Implemented pagination in ForumRepository for efficient forum retrieval with Pageable support

--- backend-java/src/main/java/com/election/backendjava/repositories/PostForumRepository.java
+++ backend-java/src/main/java/com/election/backendjava/repositories/PostForumRepository.java
@@ -1,13 +1,13 @@
 package com.election.backendjava.repositories;
 
 import com.election.backendjava.entities.UserForum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PostForumRepository extends JpaRepository<UserForum, Long> {
-    @Query("SELECT f FROM UserForum f ORDER BY f.createdAt DESC")
-    List<UserForum> findAllSortedByCreatedAt();
-
+    Page<UserForum> findAll(Pageable pageable);
 }
