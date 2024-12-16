d63fad11a0e13c769dc3aba202991e2894fd95e6
khaouia <khaouia@hva.nl>
2024-12-16 18:39:54 +0100

    method to get the requests

--- backend-java/src/main/java/com/election/backendjava/services/UnbanRequestService.java
+++ backend-java/src/main/java/com/election/backendjava/services/UnbanRequestService.java
@@ -1,4 +1,19 @@
 package com.election.backendjava.services;
 
+import com.election.backendjava.entities.UnbanRequest;
+import com.election.backendjava.repositories.UnbanRequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UnbanRequestService {
+
+    @Autowired
+    private UnbanRequestRepository unbanRequestRepository;
+
+    public List<UnbanRequest> getAllUnbanRequests() {
+        return unbanRequestRepository.findAll();
+    }
 }
