9c4df7f3ddc636f49aa038e964fb4b231a8784ec
Darya Masoumi <Darya.masoumi@hva.nl>
2024-12-16 23:48:14 +0100

    Added pagination to ForumController

--- backend-java/src/main/java/com/election/backendjava/controllers/PostForumController.java
+++ backend-java/src/main/java/com/election/backendjava/controllers/PostForumController.java
@@ -4,12 +4,13 @@ import com.election.backendjava.entities.Reply;
 import com.election.backendjava.entities.UserForum;
 import com.election.backendjava.services.ForumService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/usersforum")
@@ -24,13 +25,14 @@ public class PostForumController {
     }
 
     @GetMapping
-    public List<UserForum> getAllForums() {
-        return forumService.getAllForums();
+    public ResponseEntity<Page<UserForum>> getAllForums(@RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<UserForum> forums = forumService.getAllForums(page);
+        return ResponseEntity.ok(forums);
     }
 
     @PostMapping("/{forumId}/replies")
-    public Reply saveReply(@PathVariable Long forumId, @RequestBody Reply reply) {
-        UserForum userForum = forumService.getAllForums().stream()
+    public Reply saveReply(@PathVariable Long forumId, @RequestBody Reply reply, @RequestParam(value = "page", defaultValue = "0") int page) {
+        UserForum userForum = forumService.getAllForums(page).stream()
                 .filter(forum -> forum.getForumId().equals(forumId))
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("Forum not found"));
@@ -44,7 +46,6 @@ public class PostForumController {
             List<Reply> replies = forumService.getRepliesByForumId(forumId);
             return ResponseEntity.ok(replies);
         } catch (Exception e) {
-            // Log the error
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
@@ -53,10 +54,12 @@ public class PostForumController {
     @DeleteMapping("/{forumId}")
     public ResponseEntity<Void> deleteForum(@PathVariable Long forumId) {
         try {
+            System.out.println("Deleting forum with ID: " + forumId);
             forumService.deleteForumById(forumId);
             return ResponseEntity.noContent().build();
         } catch (Exception e) {
             e.printStackTrace();
+            System.err.println("Error during deletion: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
