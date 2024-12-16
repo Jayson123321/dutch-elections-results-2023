28c2a486d2ae851f62e6f6d09940601084a5539d
khaouia <khaouia@hva.nl>
2024-12-16 18:35:24 +0100

    UnbanRequestRepository

--- backend-java/src/main/java/com/election/backendjava/repositories/UnbanRequestRepository.java
+++ backend-java/src/main/java/com/election/backendjava/repositories/UnbanRequestRepository.java
@@ -1,4 +1,9 @@
 package com.election.backendjava.repositories;
 
-public interface UnbanRequestRepository {
+import com.election.backendjava.entities.UnbanRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UnbanRequestRepository extends JpaRepository<UnbanRequest, Long> {
 }
