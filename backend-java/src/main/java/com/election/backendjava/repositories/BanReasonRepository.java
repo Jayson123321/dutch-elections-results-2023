ea19aa03a3e96c639a5d68fee07bcddfa3176cb6
khaouia <khaouia@hva.nl>
2024-12-17 19:50:33 +0100

    interface and imports

--- backend-java/src/main/java/com/election/backendjava/repositories/BanReasonRepository.java
+++ backend-java/src/main/java/com/election/backendjava/repositories/BanReasonRepository.java
@@ -1,4 +1,9 @@
 package com.election.backendjava.repositories;
 
-public interface BanReasonRepository {
+import com.election.backendjava.entities.BanReason;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BanReasonRepository extends JpaRepository<BanReason, Long> {
 }
