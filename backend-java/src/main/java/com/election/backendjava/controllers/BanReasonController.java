c74b49320e1fd7a278a553a433847ccfb15d729b
khaouia <khaouia@hva.nl>
2024-12-17 20:02:43 +0100

    endpoints getting and saving ban reasons

--- backend-java/src/main/java/com/election/backendjava/controllers/BanReasonController.java
+++ backend-java/src/main/java/com/election/backendjava/controllers/BanReasonController.java
@@ -1,4 +1,27 @@
 package com.election.backendjava.controllers;
 
+import com.election.backendjava.entities.BanReason;
+import com.election.backendjava.services.BanReasonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/ban-reasons")
 public class BanReasonController {
+
+    @Autowired
+    private BanReasonService banReasonService;
+
+    @GetMapping
+    public List<BanReason> getAllBanReasons() {
+        return banReasonService.getAllBanReasons();
+    }
+
+    @PostMapping
+    public void addBanReason(@RequestBody BanReason banReason) {
+        banReasonService.saveBanReason(banReason);
+    }
 }
