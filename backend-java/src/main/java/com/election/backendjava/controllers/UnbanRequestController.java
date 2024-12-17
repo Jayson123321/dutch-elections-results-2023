30f7cd99344af721470ee8466f5fc6a48c8d08ef
khaouia <khaouia@hva.nl>
2024-12-17 17:37:59 +0100

    Controller to reject/delete request

--- backend-java/src/main/java/com/election/backendjava/controllers/UnbanRequestController.java
+++ backend-java/src/main/java/com/election/backendjava/controllers/UnbanRequestController.java
@@ -33,5 +33,8 @@ public class UnbanRequestController {
         }
     }
 
-
+    @DeleteMapping("/{id}/reject")
+    public void rejectUnbanRequest(@PathVariable Long id) {
+        unbanRequestService.deleteUnbanRequest(id);
+    }
 }
