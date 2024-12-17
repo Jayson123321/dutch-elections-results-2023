283d50bcdf6cbfaf623dba3ef4748e97d275266b
khaouia <khaouia@hva.nl>
2024-12-17 17:37:07 +0100

    Controller to change user role after ticket approving

--- backend-java/src/main/java/com/election/backendjava/controllers/UnbanRequestController.java
+++ backend-java/src/main/java/com/election/backendjava/controllers/UnbanRequestController.java
@@ -2,6 +2,7 @@ package com.election.backendjava.controllers;
 
 import com.election.backendjava.entities.UnbanRequest;
 import com.election.backendjava.services.UnbanRequestService;
+import com.election.backendjava.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,8 +16,22 @@ public class UnbanRequestController {
     @Autowired
     private UnbanRequestService unbanRequestService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public List<UnbanRequest> getAllUnbanRequests() {
         return unbanRequestService.getAllUnbanRequests();
     }
+
+    @PutMapping("/{id}/approve")
+    public void approveUnbanRequest(@PathVariable Long id) {
+        UnbanRequest unbanRequest = unbanRequestService.findById(id);
+        if (unbanRequest != null) {
+            userService.unbanUser(unbanRequest.getUser().getId());
+            unbanRequestService.deleteUnbanRequest(id);
+        }
+    }
+
+
 }
