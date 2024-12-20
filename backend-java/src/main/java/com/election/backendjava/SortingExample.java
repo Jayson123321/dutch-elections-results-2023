69a4dea3e578a857d8d0414a10a045457d3aacb1
Darya Masoumi <Darya.masoumi@hva.nl>
2024-12-20 23:46:59 +0100

    sorting algorithms

---
+++ backend-java/src/main/java/com/election/backendjava/SortingExample.java
@@ -0,0 +1,140 @@
+public class SortingExample {
+
+    // Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Insertion Sort
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    // Selection Sort
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Merge Sort
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) temp[k++] = arr[i++];
+        while (j <= right) temp[k++] = arr[j++];
+
+        for (i = left, k = 0; i <= right; i++, k++) arr[i] = temp[k];
+    }
+
+    // Quick Sort
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    // Print Array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] ids = {35, 12, 43, 8, 51, 17, 24, 5, 63, 29}; // Example dataset
+
+        // Bubble Sort
+        int[] bubbleArr = ids.clone();
+        bubbleSort(bubbleArr);
+        System.out.print("Bubble Sort: ");
+        printArray(bubbleArr);
+
+        // Insertion Sort
+        int[] insertionArr = ids.clone();
+        insertionSort(insertionArr);
+        System.out.print("Insertion Sort: ");
+        printArray(insertionArr);
+
+        // Selection Sort
+        int[] selectionArr = ids.clone();
+        selectionSort(selectionArr);
+        System.out.print("Selection Sort: ");
+        printArray(selectionArr);
+
+        // Merge Sort
+        int[] mergeArr = ids.clone();
+        mergeSort(mergeArr, 0, mergeArr.length - 1);
+        System.out.print("Merge Sort: ");
+        printArray(mergeArr);
+
+        // Quick Sort
+        int[] quickArr = ids.clone();
+        quickSort(quickArr, 0, quickArr.length - 1);
+        System.out.print("Quick Sort: ");
+        printArray(quickArr);
+    }
+}
+
