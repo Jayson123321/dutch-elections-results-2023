b01c6e550fe1b45866bb69523df85e3f99639813
Jayson <jayson.haverkamp@hva.nl>
2025-01-10 19:09:25 +0100

    Add i18n translation ($t) support to ChooseResults component

--- election-frontend-vue/src/components/ChooseResults.vue
+++ election-frontend-vue/src/components/ChooseResults.vue
@@ -1,38 +1,38 @@
-<script lang="ts">
-import {defineComponent} from 'vue'
-import HeaderComponent from "@/components/HeaderComponent.vue";
-import FooterComponent from "@/components/FooterComponent.vue";
-
-export default defineComponent({
-  name: "choose-results" ,
-  components: {FooterComponent, HeaderComponent}
-})
-</script>
-
 <template>
   <div>
     <HeaderComponent />
     <div class="homepage">
-      <h1>Welkom bij Demo-Crazy</h1>
-      <p>De plek waar je alles kunt vinden over de politieke partijen in Nederland.</p>
+      <h1>{{ $t('chooseResults.welcome_message') }}</h1>
+      <p>{{ $t('chooseResults.description_message') }}</p>
       <div class="flexcontainer">
         <router-link to="/localAuthorityResults" class="flexbox flexbox1">
-          <h2>Gemeentelijke Uitslagen</h2>
-          <p>Ontdek wat jou gemeente heeft gestemd!</p>
+          <h2>{{ $t('chooseResults.local_authority_results') }}</h2>
+          <p>{{ $t('chooseResults.discover_local_votes') }}</p>
         </router-link>
         <router-link to="/managing-authorities" class="flexbox flexbox2">
-          <h2>Gemeentelijke uitslag per stembureau</h2>
-          <p>Bekijk de uitslag per stembureau</p>
+          <h2>{{ $t('chooseResults.local_results_per_polling_station') }}</h2>
+          <p>{{ $t('chooseResults.view_results_per_polling_station') }}</p>
         </router-link>
         <router-link to="/totalResults" class="flexbox flexbox3">
-          <h2>Totaal uitslag</h2>
-          <p>Bekijk de totaal uitslag</p>
+          <h2>{{ $t('chooseResults.total_results') }}</h2>
+          <p>{{ $t('chooseResults.view_total_results') }}</p>
         </router-link>
       </div>
     </div>
     <FooterComponent />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+
+export default defineComponent({
+  name: "choose-results",
+  components: { FooterComponent, HeaderComponent }
+});
+</script>
 
 <style scoped>
 
