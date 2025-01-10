59baa518e94c192b0c754c0e7316fa702f6e6377
Jayson <jayson.haverkamp@hva.nl>
2025-01-10 19:06:15 +0100

    Add i18n support with English and Dutch translations (chooseResults and totalResults translation page)

--- election-frontend-vue/src/i181n.js
+++ election-frontend-vue/src/i181n.js
@@ -9,7 +9,7 @@ const messages = {
             parties: 'Parties',
             login: 'Login',
             search: 'Search...',
-            toggle_button: 'ENG'
+            toggle_button: 'En'
         },
         header: {
             home: 'Home',
@@ -18,6 +18,19 @@ const messages = {
             login: 'Login',
             search: 'Search...',
             toggle_language: 'Toggle Language',
+        },
+        totalResults: {
+            tweede_kamer_uitslagen: 'House of Representatives Results',
+        },
+        chooseResults: {
+            welcome_message: 'Welcome to Demo-Crazy',
+            description_message: 'The place where you can find everything about political parties in the Netherlands.',
+            local_authority_results: 'Local Authority Results',
+            discover_local_votes: 'Discover what your municipality has voted!',
+            local_results_per_polling_station: 'Local results per polling station',
+            view_results_per_polling_station: 'View the results per polling station',
+            total_results: 'Total results',
+            view_total_results: 'View the total results',
         },
     },
     nl: {
@@ -36,6 +49,19 @@ const messages = {
             login: 'Inloggen',
             search: 'Zoeken...',
             toggle_language: 'Taal Wisselen',
+        },
+        totalResults: {
+            tweede_kamer_uitslagen: 'Tweede kamer uitslagen',
+        },
+        chooseResults: {
+            welcome_message: 'Welkom bij Demo-Crazy',
+            description_message: 'De plek waar je alles kunt vinden over de politieke partijen in Nederland.',
+            local_authority_results: 'Gemeentelijke Uitslagen',
+            discover_local_votes: 'Ontdek wat jouw gemeente heeft gestemd!',
+            local_results_per_polling_station: 'Gemeentelijke uitslag per stembureau',
+            view_results_per_polling_station: 'Bekijk de uitslag per stembureau',
+            total_results: 'Totaal uitslag',
+            view_total_results: 'Bekijk de totaal uitslag',
         },
     }
 };
