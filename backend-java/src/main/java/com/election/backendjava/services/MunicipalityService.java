077a961195c2cd1ada6d98268e0866e0bdeee4f5
khaouia <khaouia@hva.nl>
2025-01-11 20:57:59 +0100

    get all municipality

--- backend-java/src/main/java/com/election/backendjava/services/MunicipalityService.java
+++ backend-java/src/main/java/com/election/backendjava/services/MunicipalityService.java
@@ -1,4 +1,19 @@
 package com.election.backendjava.services;
 
+import com.election.backendjava.entities.Municipality;
+import com.election.backendjava.repositories.MunicipalityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MunicipalityService {
+
+    @Autowired
+    private MunicipalityRepository municipalityRepository;
+
+    public List<Municipality> getAllMunicipalities() {
+        return municipalityRepository.findAll();
+    }
 }
