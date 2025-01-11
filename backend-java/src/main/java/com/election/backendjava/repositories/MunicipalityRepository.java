8d9e5112efcc03d9643a771d3743d10be402f2e6
khaouia <khaouia@hva.nl>
2025-01-11 20:51:02 +0100

    repository for municipality

--- backend-java/src/main/java/com/election/backendjava/repositories/MunicipalityRepository.java
+++ backend-java/src/main/java/com/election/backendjava/repositories/MunicipalityRepository.java
@@ -1,4 +1,12 @@
 package com.election.backendjava.repositories;
 
-public class MunicipalityRepository {
+import com.election.backendjava.entities.Municipality;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MunicipalityRepository extends JpaRepository<Municipality, Long> {
+    Optional<Municipality> findByMunicipalityId(String municipalityId);
 }
