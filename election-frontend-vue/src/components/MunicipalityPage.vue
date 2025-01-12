ea035da69213da23500632ca829a751112bc780a
khaouia <khaouia@hva.nl>
2025-01-12 11:57:00 +0100

    fix front end

--- election-frontend-vue/src/components/MunicipalityPage.vue
+++ election-frontend-vue/src/components/MunicipalityPage.vue
@@ -1,14 +1,6 @@
 <script setup lang="ts">
 import HeaderComponent from '../components/HeaderComponent.vue';
 import FooterComponent from '../components/FooterComponent.vue';
-
-export default {
-  name: 'MunicipalityPage',
-  components: {
-    HeaderComponent,
-    FooterComponent,
-  },
-};
 </script>
 
 <template>
