abb4f2da7901a43296b83e144f5693f0e1df9248
khaouia <khaouia@hva.nl>
2025-01-12 11:11:38 +0100

    end point for api

--- backend-java/src/main/java/com/election/backendjava/controllers/MunicipalityController.java
+++ backend-java/src/main/java/com/election/backendjava/controllers/MunicipalityController.java
@@ -1,4 +1,14 @@
 package com.election.backendjava.controllers;
 
+import com.election.backendjava.entities.Municipality;
+import com.election.backendjava.services.MunicipalityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/municipalities")
 public class MunicipalityController {
 }
