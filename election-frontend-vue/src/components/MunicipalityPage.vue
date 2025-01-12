3b09c5b5bf6c3cbc4c48f49ebd989ae2cc038945
khaouia <khaouia@hva.nl>
2025-01-12 11:36:24 +0100

    header footer import

--- election-frontend-vue/src/components/MunicipalityPage.vue
+++ election-frontend-vue/src/components/MunicipalityPage.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+import HeaderComponent from '../components/HeaderComponent.vue';
+import FooterComponent from '../components/FooterComponent.vue';
+
 export default {
   name: 'MunicipalityPage',
   components: {
-  }}
+    HeaderComponent,
+    FooterComponent,
+  },
+};
 </script>
 
 <template>
