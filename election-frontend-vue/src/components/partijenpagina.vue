ec002be3f8a3fa52553d82e44c9dcc04fbbb00c9
Kaylin Bon <kaylin.bon@hva.nl>
2025-01-13 19:49:44 +0100

    fixed some bugs

--- election-frontend-vue/src/components/partijenpagina.vue
+++ election-frontend-vue/src/components/partijenpagina.vue
@@ -55,8 +55,18 @@ export default {
 
 <style>
 /* Algemene stijlen voor de container */
+
+html, body, .container {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+
 .container {
-  max-width: 1200px;
+  width: 100%;
+  height: 100%;
   padding: 20px;
   font-family: Arial, sans-serif;
   margin: 50px auto 100px auto;
