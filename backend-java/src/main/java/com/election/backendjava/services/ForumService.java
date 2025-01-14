077b034399f9954c7cbe5e8a917248ee0bb310da
Darya Masoumi <Darya.masoumi@hva.nl>
2025-01-14 18:21:47 +0100

    deleted useless comment

--- backend-java/src/main/java/com/election/backendjava/services/ForumService.java
+++ backend-java/src/main/java/com/election/backendjava/services/ForumService.java
@@ -1,66 +1,6 @@
-//package com.election.backendjava.services;
-//
-//import com.election.backendjava.entities.Reply;
-//import com.election.backendjava.entities.UserForum;
-//import com.election.backendjava.repositories.PostForumRepository;
-//import com.election.backendjava.repositories.ReplyRepository;
-//import jakarta.persistence.EntityNotFoundException;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class ForumService {
-//    private static final Logger logger = LoggerFactory.getLogger(ForumService.class);
-//
-//    @Autowired
-//    private PostForumRepository postForumRepository;
-//
-//    @Autowired
-//    private ReplyRepository replyRepository;
-//
-//    public UserForum save(UserForum form) {
-//        return postForumRepository.save(form);
-//    }
-//
-//    public Page<UserForum> getAllForums(int page) {
-//        Pageable pageable = PageRequest.of( page, 5);
-//        Page<UserForum> forums = postForumRepository.findAll(pageable);
-//        logger.info("Aantal opgehaalde forums op deze pagina: {}", forums.getNumberOfElements());
-//        return forums;
-//    }
-//
-//    public Reply saveReply(Reply reply) {
-//        return replyRepository.save(reply);
-//    }
-//
-//    public List<Reply> getRepliesByForumId(Long forumId) {
-//        return replyRepository.findByUserForum_ForumId(forumId);
-//    }
-//
-//    public void deleteForumById(Long forumId) {
-//        if (!postForumRepository.existsById(forumId)) {
-//            throw new EntityNotFoundException("Forum met ID " + forumId + " bestaat niet.");
-//        }
-//
-//        replyRepository.deleteByUserForum_ForumId(forumId);
-//
-//        postForumRepository.deleteById(forumId);
-//
-//        logger.info("Forum with ID {} and its replies have been deleted.", forumId);
-//    }
-//}
-
 package com.election.backendjava.services;
 
 import com.election.backendjava.entities.Reply;
-import com.election.backendjava.entities.User;
 import com.election.backendjava.entities.UserForum;
 import com.election.backendjava.repositories.PostForumRepository;
 import com.election.backendjava.repositories.ReplyRepository;
